4382e2ba507084a92a3ad85ff9f3a7544fd42d35
satanforbid <makipactaoin@gmail.com>
2024-04-27 04:12:12 +0700

    initial commit

---
+++ capstone/static/js/navbar.js
@@ -0,0 +1,41 @@
+const userNav = document.querySelector(".user-nav");
+const userBtn = document.querySelector("#user");
+const userDropdown = document.querySelector(".user-dropdown");
+
+const handleUserDropdown = () => {
+  userDropdown.classList.toggle("hide");
+  // console.log("clicked");
+};
+
+userBtn.addEventListener("click", handleUserDropdown);
+
+const notifyBtn = document.querySelector("#notify");
+const notifyDropdown = document.querySelector(".notify-dropdown");
+
+const handleNotifyDropdown = () => {
+  notifyDropdown.classList.toggle("hide");
+  // console.log("clicked");
+};
+
+notifyBtn.addEventListener("click", handleNotifyDropdown);
+
+const messageBtn = document.querySelector("#message");
+const messageDropdown = document.querySelector(".message-dropdown");
+
+const handleMessageDropdown = () => {
+  messageDropdown.classList.toggle("hide");
+  // console.log("clicked");
+};
+
+messageBtn.addEventListener("click", handleMessageDropdown);
+
+const settingsBtn = document.querySelector("#settings");
+const settingsDropdown = document.querySelector(".settings-dropdown");
+
+const handleSettingsDropdown = () => {
+  settingsDropdown.classList.toggle("hide");
+  // console.log("clicked");
+};
+
+settingsBtn.addEventListener("click", handleSettingsDropdown);
+
